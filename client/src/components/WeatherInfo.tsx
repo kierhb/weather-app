cbd47ae85a6fddfcfe2028665a4adfb4884c5d04
Kier Bobila <kierbobila@gmail.com>
2023-04-16 17:14:24 +0800

    Rebuild WeatherInfo

--- client/src/components/WeatherInfo.tsx
+++ client/src/components/WeatherInfo.tsx
@@ -3,14 +3,16 @@ import CloudQueueRoundedIcon from "@mui/icons-material/CloudQueueRounded";
 const WeatherInfo = () => {
   return (
     <div className="weather-info">
-      <div className="condition">Overcast Clouds</div>
-      <p className="location">Rizal, Nueva Ecija</p>
-      <p className="date">Sunday, 16th April 2023</p>
-      <p className="time">4:30 pm</p>
-
       <div className="temp">28 °C</div>
-      <div className="icon">
+
+      <div className="location-time">
+        <p className="location">Rizal, Nueva Ecija</p>
+        <p className="time-date">4:30 pm - Sunday, 16th April 2023</p>
+      </div>
+
+      <div className="condition">
         <CloudQueueRoundedIcon sx={{ fontSize: "4rem" }} />
+        Overcast Clouds
       </div>
     </div>
   );
