c185253eac61cfc6a77adfb00235023c4649601f
Kier Bobila <kierbobila@gmail.com>
2023-04-17 15:46:43 +0800

    Add additional features

--- client/src/components/WeatherInfo.tsx
+++ client/src/components/WeatherInfo.tsx
@@ -3,17 +3,25 @@ import CloudQueueRoundedIcon from "@mui/icons-material/CloudQueueRounded";
 const WeatherInfo = () => {
   return (
     <div className="weather-info">
-      <h1 className="temp">28 °C</h1>
-
-      <div className="location-time">
-        <p className="location">Rizal, Nueva Ecija</p>
-        <p className="time-date">4:30 pm - Sunday, 16th April 2023</p>
+      <div className="units">
+        <button>°C</button>
+        <p>|</p>
+        <button>°F</button>
       </div>
 
-      <h2 className="condition">
-        <CloudQueueRoundedIcon sx={{ fontSize: "4rem" }} />
-        Overcast Clouds
-      </h2>
+      <div className="display">
+        <h1 className="temp">28 °C</h1>
+
+        <div className="location-time">
+          <p className="location">Rizal, Nueva Ecija</p>
+          <p className="time-date">4:30 pm - Sunday, 16th April 2023</p>
+        </div>
+
+        <h2 className="condition">
+          <CloudQueueRoundedIcon sx={{ fontSize: "4rem" }} />
+          Overcast Clouds
+        </h2>
+      </div>
     </div>
   );
 };
